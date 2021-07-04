a962aa1c76ea7de3d16e471ceb2faa680e2a8f58
Saecki <tobiasschmitz2001@gmail.com>
2021-07-04 17:07:08 +0200

    add basic functionality

--- gtk/src/main.rs
+++ gtk/src/main.rs
@@ -1,6 +1,8 @@
 use gtk4::prelude::*;
 use gtk4::Application;
 use gtk4::ApplicationWindow;
+use gtk4::Orientation;
+use gtk4::Text;
 use gtk4::TextView;
 
 fn main() {
@@ -18,19 +20,38 @@ fn build_ui(app: &Application) {
         .default_height(600)
         .build();
 
-    let text_view = TextView::builder()
-        .margin_top(12)
-        .margin_end(12)
-        .margin_bottom(12)
-        .margin_start(12)
+    let output = Text::new();
+    let input = TextView::new();
+
+    let container = gtk4::Box::builder()
+        .orientation(Orientation::Vertical)
         .build();
+    container.append(&output);
+    container.append(&input);
+    window.set_child(Some(&container));
 
-    let buffer = text_view.buffer();
+    let buffer = input.buffer();
+    buffer.connect_changed(move |t| {
+        let gs = t.slice(&t.start_iter(), &t.end_iter(), true);
+        let s = gs.as_str();
 
-    buffr.connect_changed(move || {
-        println!("changed");
+        match cods::calc(s) {
+            Ok((v, warnings)) => {
+                for _w in warnings.iter() {
+                    // string.push_str(&w.display(s).to_string());
+                }
+
+                output.set_text(&v.to_string());
+            }
+            Err(errors) => {
+                for _e in errors.iter() {
+                    // string.push_str(&e.display(s).to_string());
+                }
+
+                output.set_text("");
+            }
+        }
     });
 
-    window.set_child(Some(&text_view));
     window.present();
 }
