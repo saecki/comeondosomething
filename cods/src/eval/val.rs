d49b4d499633fa9fcbcdb69cb8a8b7c09cda65ae
Saecki <tobiasschmitz2001@gmail.com>
2022-03-24 11:10:42 +0100

    fix: panic when variable isn't defined

--- cods/src/eval/val.rs
+++ cods/src/eval/val.rs
@@ -32,21 +32,30 @@ impl Context {
     pub fn to_val<'a>(&'a self, expr: &'a Expr) -> crate::Result<&'a Val> {
         match &expr.typ {
             ExprT::Val(p) => Ok(p),
-            ExprT::Var(id) => {
-                let var = self.var(*id);
-                match &var.value {
-                    Some(d) => Ok(d),
-                    None => {
-                        let name = self.ident_name(*id);
-                        Err(crate::Error::UndefinedVar(name.to_owned(), expr.range))
-                    }
+            ExprT::Var(id) => match self.var_val(*id) {
+                Some(d) => Ok(d),
+                None => {
+                    let name = self.ident_name(*id);
+                    Err(crate::Error::UndefinedVar(name.to_owned(), expr.range))
                 }
-            }
+            },
         }
     }
 
-    pub fn var(&self, id: Ident) -> &Var {
+    pub fn var(&self, id: Ident) -> Option<&Var> {
         self.scope.var(id)
+    }
+
+    pub fn var_mut(&mut self, id: Ident) -> Option<&mut Var> {
+        self.scope.var_mut(id)
+    }
+
+    pub fn var_val(&self, id: Ident) -> Option<&Val> {
+        self.scope.val(id)
+    }
+
+    pub fn var_val_mut(&mut self, id: Ident) -> Option<&mut Val> {
+        self.scope.val_mut(id)
     }
 
     pub fn set_var(&mut self, id: Ident, val: Option<Val>) {
@@ -65,8 +74,20 @@ impl Scope {
         self.vars.clear();
     }
 
-    pub fn var(&self, id: Ident) -> &Var {
-        &self.vars[&id]
+    pub fn var(&self, id: Ident) -> Option<&Var> {
+        self.vars.get(&id)
+    }
+
+    pub fn var_mut(&mut self, id: Ident) -> Option<&mut Var> {
+        self.vars.get_mut(&id)
+    }
+
+    pub fn val(&self, id: Ident) -> Option<&Val> {
+        self.var(id).and_then(|v| v.value.as_ref())
+    }
+
+    pub fn val_mut(&mut self, id: Ident) -> Option<&mut Val> {
+        self.var_mut(id).and_then(|v| v.value.as_mut())
     }
 
     pub fn set_var(&mut self, id: Ident, val: Option<Val>) {
