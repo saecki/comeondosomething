96327789a92b5e478f89dc19f095aabe1d10240d
Saecki <tobiasschmitz2001@gmail.com>
2022-05-20 15:17:42 +0200

    test: cli output

---
+++ cli/tests/test.rs
@@ -0,0 +1,31 @@
+use assert_cmd::Command;
+
+#[test]
+fn dyn_capture_output() {
+    let input = "\
+fun outer() {
+    val a = 3
+
+    fun inner() -> int {
+        a
+    }
+}
+";
+    let output = "\
+[1;94m2 │[0m     val a = 3
+  [1;94m│[0m         [1;91m^[0m    
+[1;94m3 │[0m 
+[1;94m4 │[0m     fun inner() -> int {
+[1;94m5 │[0m         a
+  [1;94m│[0m         [1;91m^[0m
+  [1;94m│[0m [1;91mCapturing variables from a dynamic scope is not yet implemented[0m[0m
+
+";
+
+    Command::cargo_bin("cods")
+        .unwrap()
+        .arg(input)
+        .assert()
+        .success()
+        .stdout(output);
+}
